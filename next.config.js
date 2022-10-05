8de83bdb3275ed158ae6fe534a01256debe93426
Valentine <valentine@zpoken.io>
2022-10-05 13:29:05 +0300

    fix wasm congfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable operator-linebreak */
 const nextTranslate = require('next-translate');
-
 module.exports = nextTranslate({
   poweredByHeader: false,
-
+  future: {
+    webpack5: false,
+  },
+  cleanDistDir: false,
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -12,9 +14,10 @@ module.exports = nextTranslate({
     config.experiments = { asyncWebAssembly: true };
     if (isServer) {
       config.output.webassemblyModuleFilename =
-        './../server/static/wasm/[modulehash].wasm';
+          './../server/static/wasm/[modulehash].wasm';
     } else {
-      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+      config.output.webassemblyModuleFilename =
+          'server/static/wasm/[modulehash].wasm';
     }
     return config;
   },
